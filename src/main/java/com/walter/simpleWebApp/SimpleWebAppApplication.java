95e8ed72429304d8067096708223de8f0e60f876
Walter9472 <Walter.Mtanda@Student.HTW-Berlin.de>
2025-08-20 20:02:18 +0200

    first commit

---
+++ src/main/java/com/walter/simpleWebApp/SimpleWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.walter.simpleWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleWebAppApplication.class, args);
+	}
+
+}
